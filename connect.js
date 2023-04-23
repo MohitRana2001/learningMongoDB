ced754fb970c2786c7e902693be05c34991439e2
MOHIT RANA <awesomemohit20@gmail.com>
2023-04-23 22:53:12 +0530

    trying differenrt things

--- connect.js
+++ connect.js
@@ -11,14 +11,28 @@ mongoose.connect(process.env.MONGO_URI)
 
 const User = mongoose.model('User', userSchema);
 
-const userInstance = new User({
-    email : "mohit@gmail.com",
-    password : "Mohit@2002"
-});
+// const userInstance = new User({
+//     email : "rana@gmail.com",
+//     password : "Mohit@2003"
+// });
 
-userInstance.save()
-.then( () => {
-    process.exit(0);
-});
+// User.find({}, 'password')
+// .then((data) => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.error(err);
+// });
+
+// User.findOneAndUpdate({email:'mohit@gmail.com'}, {password: 'Mohit@2004'})
+// .then((data) => console.log(data.password))
+// .then(() => {
+//     process.exit(0);
+// })
+// .catch(() => console.log("couldn't find the password"));
+
+// userInstance.save()
+// .then( () => {
+//     process.exit(0);
+// });
 
 module.exports = mongoose.connection;
