a1b826439f755be799241566d7f8fe39a375457a
MOHIT RANA <awesomemohit20@gmail.com>
2023-04-23 22:53:43 +0530

    Trying to get user's details

--- app.js
+++ app.js
@@ -3,16 +3,35 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const mongoose = require("mongoose");
-// const connection = require("./connect");
-const port = process.env.PORT;
+const connection = require("./connect");
+const userSchema = require("./models/users");
+const port = 3000;
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(bodyParser.json());
 
+const User = mongoose.model('User', userSchema);
+
 app.get('/', (req,res) => {
-    res.send("Learning in progress");
+  res.send("Hello there!!!");
 });
+
+app.post('/', (req,res) => {
+  const user = req.body.user;
+  const password = req.body.pass;
+
+  const newUser = new User({user, pass : password});
+
+  newUser.save()
+  .then(() => res.send("new user saved succeddfully"))
+  .then(() => res.redirect('/success'))
+  .catch((err) => {
+    res.send(err);
+  });
+  res.redirect()
+  // res.send("We recieeved your credentials");
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
